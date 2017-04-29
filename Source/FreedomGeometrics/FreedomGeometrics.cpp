66210fe9f98e71149c7f981c53f3383813943312
Zach Falcone <zac.falcone@gmail.com>
2017-04-28 21:05:50 -0700

    Added project files.
    Created project files for Freedom Geometrics.

---
+++ Source/FreedomGeometrics/FreedomGeometrics.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FreedomGeometrics.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FreedomGeometrics, "FreedomGeometrics" );
